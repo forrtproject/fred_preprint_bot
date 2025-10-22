684195c4fd3499414d135d1be412981db6fad047
Eliska Blazek <71393504+eli-blazek@users.noreply.github.com>
2025-10-22 17:32:53 +0200

    Added a function that extracts emails from a pdf file. Does not work yet.

---
+++ extraction/get_mail_from_pdf.py
@@ -0,0 +1,28 @@
+import re
+from pypdf import PdfReader
+
+
+def get_emails_from_pdf(path):
+    doc = PdfReader(path)
+    
+
+    emails = []
+    auth_email = r"^\\S+@\\S+\\.\\S+$"
+
+
+    for page in doc.pages:
+        text = page.extract_text()
+        # Find all matches of the pattern
+        page_matches = re.findall(auth_email, text, re.IGNORECASE)
+        
+        for match in page_matches:
+            emails.append(match)    
+    return emails 
+
+
+get_emails_from_pdf('tester.pdf')
+
+
+
+
+
