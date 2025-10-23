4a9a4d1f6947cb8b15aa457230eb5350635a9b65
Eliska Blazek <71393504+eli-blazek@users.noreply.github.com>
2025-10-23 16:31:24 +0200

    Added Orcid scraping functionality from PDF.

--- extraction/extract_orcids_from_pdfs.py
+++ extraction/extract_orcids_from_pdfs.py
@@ -1,4 +1,4 @@
-from get_mail_from_pdf import get_mail_from_pdf
+from get_orcid_from_pdf import get_orcid_from_pdf
 from os import listdir, path
 from enum import Enum
 import json
@@ -11,28 +11,30 @@ class Sources(Enum):
     SOCARXIV = "socarxiv"
     THESISCOMM = "thesiscommons"
 
-# Function that crawls all of preprints and mines emails from them. If an email is there, this function should find it. 
+# Function that crawls all of preprints and mines ORCiD from them.
 # Warning: It runs for a long time. 
-# Outputs into a json file called "emails_from_pdf.json"
+# Outputs into a json file called "orcids_from_pdf.json"
 
 root_path = path.abspath("../data/preprints") # The default if the function is called from it's current position.
 
-def get_mails_from_all_pdfs(root_path=path.abspath("../data/preprints")):
+def get_orcids_from_all_pdfs(root_path=path.abspath("../data/preprints")):
     file_name = "file.pdf" # This is a convension held by the preprint bot
 
-    structure_that_holds_author_emails = {}
+    structure_that_holds_author_orcids = {}
 
     for source in Sources:
         source_path = path.join(root_path, source.value)
         nested_folders = listdir(source_path)
         for folder in nested_folders:
             curr_path = path.join(source_path, folder, file_name)
-            emails_from_paper = get_mail_from_pdf(curr_path)
-            if not emails_from_paper:
-                structure_that_holds_author_emails[folder] = "false" 
+            orcids_from_paper = get_orcid_from_pdf(curr_path)
+            if not orcids_from_paper:
+                structure_that_holds_author_orcids[folder] = "false" 
             else:
-                structure_that_holds_author_emails[folder] = emails_from_paper
+                structure_that_holds_author_orcids[folder] = orcids_from_paper
                 
 
-    with open('emails_from_pdf.json', 'w') as fp:
-        json.dump(structure_that_holds_author_emails, fp)
+    with open('orcids_from_pdf.json', 'w') as fp:
+        json.dump(structure_that_holds_author_orcids, fp)
+
+get_orcids_from_all_pdfs()
