b548568b94cba6a01fd82795673c5bbdc4430bb0
Nishka Dasgupta <nid@dmi.dk>
2025-10-24 13:02:37 +0200

    Cleanup of OpenAlex API calls
    Add code to call the OpenAlex checker after CrossRef.
    Minor cleanup in the OpenAlex checker code and adjustments to only run on what CrossRef could not find.

--- scripts/augmentation/run_augmentation.sh
+++ scripts/augmentation/run_augmentation.sh
@@ -4,5 +4,7 @@
 python scripts/augmentation/extract_preprints_and_references.py --data-dir data/preprints --output data/preprints_with_references.json --verbose
 
 # Look for these on CrossRef
-
 python scripts/augmentation/matching_crossref.py
+
+#Look for whatever was not available on CrossRef, on OpenAlex
+python scripts/augmentation/doi_check_openalex.py
